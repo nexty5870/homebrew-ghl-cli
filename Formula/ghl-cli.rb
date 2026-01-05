058a191c9c1147b9d563f55dae56ec901b2462e5
Quentin <quentin.daems@gmail.com>
2026-01-06 00:34:56 +0100

    Update formula for nexty5870

--- Formula/ghl-cli.rb
+++ Formula/ghl-cli.rb
@@ -1,8 +1,8 @@
 # Homebrew formula for ghl-cli
 class GhlCli < Formula
   desc "GoHighLevel CLI - Manage your GHL account from the terminal"
-  homepage "https://github.com/shvz/ghl-cli"
-  url "https://github.com/shvz/ghl-cli/archive/refs/tags/v1.0.0.tar.gz"
+  homepage "https://github.com/nexty5870/ghl-cli"
+  url "https://github.com/nexty5870/ghl-cli/archive/refs/tags/v1.0.0.tar.gz"
   sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   license "ISC"
 
